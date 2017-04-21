ffd8fb2761474079f0efcf0e0447bd7179eafa8d
Joseal Pantoja <joseal.pantoja@gmail.com>
2017-04-21 18:52:50 -0500

    Initial Commit

---
+++ HolaiPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaiPad
+//
+//  Created by Joseal Pantoja on 21/04/17.
+//  Copyright © 2017 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
